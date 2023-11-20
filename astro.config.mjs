2447bdbbf57cf88d37a372296ae13eba2285ec71
MarcinParda <marcin98parda@gmail.com>
2023-11-20 23:32:41 +0100

    feat: implement i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()]
+  site: 'https://example.com',
+  integrations: [preact()],
+  prefetch: {
+    prefetchAll: true,
+  },
+  experimental: {
+    i18n: {
+      locales: ['en', 'pl'],
+      defaultLocale: 'en',
+      routingStrategy: 'prefix-other-locales',
+      fallback: {
+        pl: 'en',
+      },
+    },
+  },
 });
