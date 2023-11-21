9409473e4e03929bf0100377dd2db7d767b5f872
MarcinParda <marcin98parda@gmail.com>
2023-11-21 22:14:01 +0100

    feat: add prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
