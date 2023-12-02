5423072ed61b5a28ffee61e257b1284df6a37b77
MarcinParda <marcin98parda@gmail.com>
2023-12-03 00:19:46 +0100

    fix: added 0 for 1-9 digits in month/day

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,17 @@
 export const getMonthAndDay = (date: Date) => {
   const month = new Date(date).getMonth() + 1;
   const day = new Date(date).getDate();
-  return `${month}/${day}`;
+  const monthWithZero = month.toString().padStart(2, "0");
+  const dayWithZero = day.toString().padStart(2, "0");
+  return `${monthWithZero}/${dayWithZero}`;
 };
 
 export const getFullDate = (date: Date) => {
   const year = new Date(date).getFullYear();
   const month = new Date(date).getMonth() + 1;
   const day = new Date(date).getDate();
-  return `${year}/${month}/${day}`;
+  const yearWithZero = year.toString().padStart(2, "0");
+  const monthWithZero = month.toString().padStart(2, "0");
+  const dayWithZero = day.toString().padStart(2, "0");
+  return `${yearWithZero}/${monthWithZero}/${dayWithZero}`;
 };
