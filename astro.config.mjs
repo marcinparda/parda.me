3364dd755af5fced813282d4a83897269b68bc7f
MarcinParda <marcin98parda@gmail.com>
2023-12-02 02:39:19 +0100

    fix: redirecting to about when / page is reached

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://parda.me",
   integrations: [tailwind()],
+  redirects: {
+    "/": "/about",
+  },
   prefetch: {
     prefetchAll: true,
   },
