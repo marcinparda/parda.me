aae310b8174f62fa12de7c7882f446316014dee7
MarcinParda <marcin98parda@gmail.com>
2023-12-03 18:14:10 +0100

    fix: build with redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://parda.me",
   integrations: [tailwind()],
-  redirects: {
-    "/": "/about",
+  "/": {
+    status: 302,
+    destination: "/about",
   },
   prefetch: {
     prefetchAll: true,
