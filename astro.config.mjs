0a896a6fbe4d313531f0a16d5ecb7f14e9c6c712
MarcinParda <marcin98parda@gmail.com>
2023-12-03 22:26:06 +0100

    fix: delete experimental i18n to fix redirecting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,10 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://parda.me",
   integrations: [tailwind()],
+  redirects: {
+    "/": "/about",
+  },
   prefetch: {
     prefetchAll: true,
-  },
-  experimental: {
-    i18n: {
-      locales: ["en", "pl"],
-      defaultLocale: "en",
-      routingStrategy: "prefix-other-locales",
-      fallback: {
-        pl: "en",
-      },
-    },
   },
 });
