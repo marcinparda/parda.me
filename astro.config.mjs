30991af66d1036b4a1aab805b40221beceb1d6ac
MarcinParda <marcin98parda@gmail.com>
2025-01-18 18:08:40 +0100

    feat: Add Vercel adapter and redirect root to about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercelAdapter from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://parda.me",
   integrations: [tailwind()],
-  redirects: {
-    "/": "/about",
-  },
-  prefetch: {
-    prefetchAll: true,
-  },
+  output: "server",
+  adapter: vercelAdapter(),
 });
