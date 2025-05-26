fec5a025ceb4fd85152f6939e0d8c472871faced
Marcin.Parda <Marcin.Parda@gds.ey.com>
2025-05-26 12:04:31 +0200

    chore: update astro.config.mjs for configuration consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 
@@ -7,4 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   output: "static",
   adapter: vercel(),
+  image: {
+    service: passthroughImageService(),
+  },
 });
